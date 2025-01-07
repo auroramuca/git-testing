fa20ac8fdfc3e50fee7a37a60df94c65c251cbb3
Aurora Muca <aurmuc18@htl-shkoder.com>
2025-01-07 10:24:43 +0100

    test commit

--- src/test/java/org/example/AccountTest.java
+++ src/test/java/org/example/AccountTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTest {
+
     @Test
     void deposit() {
         Account account = new Account(100);
